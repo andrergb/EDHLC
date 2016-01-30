e70d99510950ad1da9278877cc8595b83fcee257
Andre Barros <andre815@gmail.com>
2016-01-30 13:42:27 -0200

    Updated PlayerList - SC

--- app/src/main/java/com/android/argb/edhlc/ExpandableListAdapter.java
+++ app/src/main/java/com/android/argb/edhlc/ExpandableListAdapter.java
@@ -2,6 +2,7 @@ package com.android.argb.edhlc;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Typeface;
 import android.support.v7.widget.CardView;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -54,29 +55,30 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
             convertView = mLayoutInflater.inflate(R.layout.expandable_list_footer, null);
         }
 
+        //Child background
+        LinearLayout main = (LinearLayout) convertView.findViewById(R.id.linearMainFooter);
+        main.setBackgroundColor(convertView.getResources().getColor(R.color.gray300));
+
+        //Child text: deck name
         TextView txtListChild = (TextView) convertView.findViewById(R.id.lblListItem);
         txtListChild.setText(footerText);
         txtListChild.setFocusable(false);
 
-        LinearLayout main = (LinearLayout) convertView.findViewById(R.id.linearMainFooter);
-        main.setBackgroundColor(convertView.getResources().getColor(R.color.gray300));
-
-        CardView cardView = (CardView) convertView.findViewById(R.id.cardView);
-
-        //Adjust last margin
+        //Last child margin
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         Resources r = mContext.getResources();
-        int dp4 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, r.getDisplayMetrics());
+        int dp2 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, r.getDisplayMetrics());
         int dp14 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 14, r.getDisplayMetrics());
         int dp16 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16, r.getDisplayMetrics());
         if (isLastChild)
-            params.setMargins(dp14, dp4, dp14, dp16);
+            params.setMargins(dp14, dp2, dp14, dp16);
         else
-            params.setMargins(dp14, dp4, dp14, dp4);
+            params.setMargins(dp14, dp2, dp14, dp2);
+        CardView cardView = (CardView) convertView.findViewById(R.id.cardView);
         cardView.setLayoutParams(params);
 
-
-        List<ImageView> imageViewList = new ArrayList<>();
+        //Deck identity
+        List<ImageView> listIdentityHolder = new ArrayList<>();
         ImageView imageViewMana1 = (ImageView) convertView.findViewById(R.id.imageViewMana1);
         ImageView imageViewMana2 = (ImageView) convertView.findViewById(R.id.imageViewMana2);
         ImageView imageViewMana3 = (ImageView) convertView.findViewById(R.id.imageViewMana3);
@@ -91,42 +93,42 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         imageViewMana5.setVisibility(View.GONE);
         imageViewMana6.setVisibility(View.GONE);
 
-        imageViewList.add(imageViewMana6);
-        imageViewList.add(imageViewMana5);
-        imageViewList.add(imageViewMana4);
-        imageViewList.add(imageViewMana3);
-        imageViewList.add(imageViewMana2);
-        imageViewList.add(imageViewMana1);
+        listIdentityHolder.add(imageViewMana6);
+        listIdentityHolder.add(imageViewMana5);
+        listIdentityHolder.add(imageViewMana4);
+        listIdentityHolder.add(imageViewMana3);
+        listIdentityHolder.add(imageViewMana2);
+        listIdentityHolder.add(imageViewMana1);
 
         int index = 0;
         if (footerIdentity.charAt(0) == '1') {
-            imageViewList.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_white));
-            imageViewList.get(index).setVisibility(View.VISIBLE);
+            listIdentityHolder.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_white));
+            listIdentityHolder.get(index).setVisibility(View.VISIBLE);
             index++;
         }
         if (footerIdentity.charAt(1) == '1') {
-            imageViewList.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_blue));
-            imageViewList.get(index).setVisibility(View.VISIBLE);
+            listIdentityHolder.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_blue));
+            listIdentityHolder.get(index).setVisibility(View.VISIBLE);
             index++;
         }
         if (footerIdentity.charAt(2) == '1') {
-            imageViewList.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_black));
-            imageViewList.get(index).setVisibility(View.VISIBLE);
+            listIdentityHolder.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_black));
+            listIdentityHolder.get(index).setVisibility(View.VISIBLE);
             index++;
         }
         if (footerIdentity.charAt(3) == '1') {
-            imageViewList.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_red));
-            imageViewList.get(index).setVisibility(View.VISIBLE);
+            listIdentityHolder.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_red));
+            listIdentityHolder.get(index).setVisibility(View.VISIBLE);
             index++;
         }
         if (footerIdentity.charAt(4) == '1') {
-            imageViewList.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_green));
-            imageViewList.get(index).setVisibility(View.VISIBLE);
+            listIdentityHolder.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_green));
+            listIdentityHolder.get(index).setVisibility(View.VISIBLE);
             index++;
         }
         if (footerIdentity.charAt(5) == '1') {
-            imageViewList.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_colorless));
-            imageViewList.get(index).setVisibility(View.VISIBLE);
+            listIdentityHolder.get(index).setBackground(convertView.getResources().getDrawable(R.drawable.mana_colorless));
+            listIdentityHolder.get(index).setVisibility(View.VISIBLE);
         }
 
         return convertView;
@@ -164,15 +166,29 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
             convertView = infalInflater.inflate(R.layout.expandable_list_header, null);
         }
 
+        //Background
+        LinearLayout main = (LinearLayout) convertView.findViewById(R.id.linearMainHeader);
+        main.setBackgroundColor(isExpanded ? convertView.getResources().getColor(R.color.gray300) : convertView.getResources().getColor(android.R.color.transparent));
+
+        //Expandable indicator
+        ImageView expandableIndicator = (ImageView) convertView.findViewById(R.id.iconIndicator);
+        expandableIndicator.setFocusable(false);
+        expandableIndicator.setImageDrawable(isExpanded ? convertView.getResources().getDrawable(R.drawable.arrow_down) : convertView.getResources().getDrawable(R.drawable.arrow_right));
+        expandableIndicator.setColorFilter(isExpanded ? convertView.getResources().getColor(android.R.color.black) : convertView.getResources().getColor(R.color.gray600));
+
+        //Group 1st line: player name
         TextView textHeader = (TextView) convertView.findViewById(R.id.textHeader);
         textHeader.setText(headerText);
+        textHeader.setTextColor(isExpanded ? convertView.getResources().getColor(android.R.color.black) : convertView.getResources().getColor(R.color.gray600));
 
+        //Group 2nd line: player info
         TextView textHeaderSub = (TextView) convertView.findViewById(R.id.textHeaderSub);
         textHeaderSub.setText(headerSubTex);
 
-        ImageButton buttonMore = (ImageButton) convertView.findViewById(R.id.buttonMore);
-        buttonMore.setFocusable(false);
-        buttonMore.setOnClickListener(new View.OnClickListener() {
+        //Button more - linear to expand clickable area
+        LinearLayout buttonMore1 = (LinearLayout) convertView.findViewById(R.id.buttonMore1);
+        buttonMore1.setFocusable(false);
+        buttonMore1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mContext instanceof PlayerListActivity) {
@@ -181,39 +197,17 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
             }
         });
 
-        Button iconAvatar = (Button) convertView.findViewById(R.id.iconAvatar);
-        iconAvatar.setFocusable(false);
-
-        CardView mCardView = (CardView) convertView.findViewById(R.id.cardView);
-        LinearLayout main = (LinearLayout) convertView.findViewById(R.id.linearMainHeader);
-        main.setBackgroundColor(isExpanded ? convertView.getResources().getColor(R.color.gray300) : convertView.getResources().getColor(android.R.color.transparent));
-
-        switch (groupPosition % 6) {
-            case 0:
-                iconAvatar.setBackground(convertView.getResources().getDrawable(R.drawable.mana_black));
-                mCardView.setBackgroundColor(isExpanded ? convertView.getResources().getColor(R.color.edh_mana_black) : convertView.getResources().getColor(android.R.color.background_light));
-                break;
-            case 1:
-                iconAvatar.setBackground(convertView.getResources().getDrawable(R.drawable.mana_blue));
-                mCardView.setBackgroundColor(isExpanded ? convertView.getResources().getColor(R.color.edh_mana_blue) : convertView.getResources().getColor(android.R.color.background_light));
-                break;
-            case 2:
-                iconAvatar.setBackground(convertView.getResources().getDrawable(R.drawable.mana_colorless));
-                mCardView.setBackgroundColor(isExpanded ? convertView.getResources().getColor(R.color.edh_mana_colorless) : convertView.getResources().getColor(android.R.color.background_light));
-                break;
-            case 3:
-                iconAvatar.setBackground(convertView.getResources().getDrawable(R.drawable.mana_green));
-                mCardView.setBackgroundColor(isExpanded ? convertView.getResources().getColor(R.color.edh_mana_green) : convertView.getResources().getColor(android.R.color.background_light));
-                break;
-            case 4:
-                iconAvatar.setBackground(convertView.getResources().getDrawable(R.drawable.mana_red));
-                mCardView.setBackgroundColor(isExpanded ? convertView.getResources().getColor(R.color.edh_mana_red) : convertView.getResources().getColor(android.R.color.background_light));
-                break;
-            case 5:
-                iconAvatar.setBackground(convertView.getResources().getDrawable(R.drawable.mana_white));
-                mCardView.setBackgroundColor(isExpanded ? convertView.getResources().getColor(R.color.edh_mana_white) : convertView.getResources().getColor(android.R.color.background_light));
-                break;
-        }
+        //Button more - image
+        ImageView buttonMore2 = (ImageView) convertView.findViewById(R.id.buttonMore2);
+        buttonMore2.setFocusable(false);
+        buttonMore2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mContext instanceof PlayerListActivity) {
+                    ((PlayerListActivity) mContext).onClickButtonMoreHeader(headerText, groupPosition);
+                }
+            }
+        });
 
         return convertView;
     }
