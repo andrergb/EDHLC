3a25479cbffa6c25b1c78d8e928b582651008637
andrergb <andre815@gmail.com>
2016-03-10 16:22:14 -0300

    Starting MainNew

---
+++ app/src/main/java/com/android/argb/edhlc/activities/MainActivityNew.java
@@ -0,0 +1,224 @@
+package com.android.argb.edhlc.activities;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+
+import com.android.argb.edhlc.Constants;
+import com.android.argb.edhlc.R;
+import com.android.argb.edhlc.Utils;
+
+public class MainActivityNew extends AppCompatActivity {
+
+    ///Drawer
+    private DrawerLayout mPlayerDrawerLayout;
+    private LinearLayout mPlayerDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Switch switchScreen;
+
+    private ActionBar mActionBar;
+    private Menu optionMenu;
+    private View statusBarBackground;
+
+    @Override
+    public void onBackPressed() {
+        if (mPlayerDrawerLayout.isDrawerOpen(mPlayerDrawer))
+            mPlayerDrawerLayout.closeDrawers();
+        else {
+            super.onBackPressed();
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+        }
+    }
+
+    public void onClickDrawerItem(View view) {
+        switch (view.getId()) {
+            case R.id.drawerItemHome:
+                mPlayerDrawerLayout.closeDrawers();
+                Intent intentHome = new Intent(this, MainActivity.class);
+                intentHome.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intentHome);
+                this.finish();
+                break;
+
+            case R.id.drawerItemPlayers:
+                mPlayerDrawerLayout.closeDrawers();
+                Intent intentPlayerList = new Intent(this, PlayerListActivity.class);
+                intentPlayerList.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intentPlayerList);
+                this.finish();
+                break;
+
+            case R.id.drawerItemRecords:
+                mPlayerDrawerLayout.closeDrawers();
+                startActivity(new Intent(this, RecordsActivity.class));
+                finish();
+                break;
+
+            case R.id.drawerItemScreen:
+                switchScreen.setChecked(!switchScreen.isChecked());
+                if (!switchScreen.isChecked()) {
+                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                    getSharedPreferences(Constants.PREFERENCE_NAME, MODE_PRIVATE).edit().putInt(Constants.SCREEN_ON, 0).commit();
+                } else {
+                    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                    getSharedPreferences(Constants.PREFERENCE_NAME, MODE_PRIVATE).edit().putInt(Constants.SCREEN_ON, 1).commit();
+                }
+                break;
+
+            case R.id.drawerItemSettings:
+                mPlayerDrawerLayout.closeDrawers();
+                startActivity(new Intent(this, SettingsActivity.class));
+                finish();
+                break;
+
+            case R.id.drawerItemAbout:
+                mPlayerDrawerLayout.closeDrawers();
+                break;
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.optionMenu = menu;
+        getMenuInflater().inflate(R.menu.menu_player_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_new);
+
+        createStatusBar();
+        createToolbar();
+        createDrawer();
+        createLayout();
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPagerMain);
+        viewPager.setAdapter(new MainFragmentPagerAdapter(getSupportFragmentManager(), MainActivityNew.this));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (getSharedPreferences(Constants.PREFERENCE_NAME, MODE_PRIVATE).getInt(Constants.SCREEN_ON, 0) == 1) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            switchScreen.setChecked(true);
+        } else {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            switchScreen.setChecked(false);
+        }
+
+    }
+
+    private void createDrawer() {
+        mPlayerDrawer = (LinearLayout) findViewById(R.id.main_drawer);
+        mPlayerDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mPlayerDrawerLayout, R.string.app_name, R.string.app_name) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        mPlayerDrawerLayout.setDrawerListener(mDrawerToggle);
+
+//        LinearLayout drawerItemPlayers = (LinearLayout) findViewById(R.id.drawerItemPlayers);
+//        ImageView drawerItemIconPlayers = (ImageView) findViewById(R.id.drawerItemIconPlayers);
+//        TextView drawerItemTextPlayers = (TextView) findViewById(R.id.drawerItemTextPlayers);
+//        drawerItemPlayers.setBackgroundColor(ContextCompat.getColor(this, R.color.gray200));
+//        drawerItemIconPlayers.setColorFilter(ContextCompat.getColor(this, R.color.accent_color));
+//        drawerItemTextPlayers.setTextColor(ContextCompat.getColor(this, R.color.accent_color));
+    }
+
+    private void createLayout() {
+        //Drawer
+        switchScreen = (Switch) findViewById(R.id.switchScreen);
+        switchScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!switchScreen.isChecked()) {
+                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                    getSharedPreferences(Constants.PREFERENCE_NAME, MODE_PRIVATE).edit().putInt(Constants.SCREEN_ON, 0).commit();
+                } else {
+                    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                    getSharedPreferences(Constants.PREFERENCE_NAME, MODE_PRIVATE).edit().putInt(Constants.SCREEN_ON, 1).commit();
+                }
+            }
+        });
+    }
+
+    private void createStatusBar() {
+        statusBarBackground = findViewById(R.id.statusBarBackground);
+        ViewGroup.LayoutParams params = statusBarBackground.getLayoutParams();
+        params.height = Utils.getStatusBarHeight(this);
+        statusBarBackground.setLayoutParams(params);
+        statusBarBackground.setBackgroundColor(ContextCompat.getColor(this, R.color.primary_color));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = this.getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.black_transparent));
+        }
+    }
+
+    private void createToolbar() {
+        assert getSupportActionBar() != null;
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        mActionBar = getSupportActionBar();
+        mActionBar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.primary_color)));
+        mActionBar.setDisplayShowTitleEnabled(true);
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mActionBar.setHomeButtonEnabled(true);
+        mActionBar.setTitle("a");
+    }
+}
---
+++ app/src/main/java/com/android/argb/edhlc/activities/MainFragmentPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.android.argb.edhlc.activities;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MainFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 4;
+    private String tabTitles[] = new String[]{"Tab1", "Tab2", "Tab3", "Tab4"};
+    private Context context;
+
+    public MainFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return MainFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+}
